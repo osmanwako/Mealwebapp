e13e7965551a6bc6f84277d71f381ed7c5251d91
OsmanFP <osmanwako411@gmail.com>
2023-04-28 00:59:30 +0300

    Adding dynamic element to html

---
+++ src/module/eventlistener.js
@@ -0,0 +1,11 @@
+const addaction = () => {
+  const comments = document.querySelectorAll('.meal-comment');
+  comments.forEach((comment) => {
+    //comment.addEventListener('click', addcomment);
+  });
+  const likes = document.querySelectorAll('.meal-like');
+  likes.forEach((like) => {
+    //like.addEventListener('click', addlike);
+  });
+};
+export default addaction;
