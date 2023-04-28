e585b128e8a80ae3976d8419d5c8531aea601856
OsmanFP <osmanwako411@gmail.com>
2023-04-29 00:20:47 +0300

    Add comment popup interaction

--- src/module/popup.js
+++ src/module/popup.js
@@ -1,5 +1,8 @@
 import { body, cpopup } from './dom_elements.js';
+<<<<<<< HEAD
 
+=======
+>>>>>>> fa87408 (Add comment popup interaction)
 export const togglepopup = () => {
   cpopup.classList.toggle('d-none');
   body.classList.toggle('d-popup');
